fd43e902549369c78ff3c266f4f824046b01ec2d
amikia <amirkian81@gmail.com>
2026-08-12 23:36:23 +0330

    fix : register services and mapping services

---
+++ Questionnaire.Application/Registers/Mappings.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Questionnaire.DataAccess;
+using Questionnaire.SharedKernel.Services;
+using System.Reflection;
+
+namespace Questionnaire.Application.Registers;
+
+public static class Mappings
+{
+    public static IServiceCollection RegisterMappingService(this IServiceCollection services, params Assembly[] assemblies)
+    {
+        if (assemblies is null || assemblies.Length == 0)
+            assemblies = new[] { Assembly.GetCallingAssembly() };
+
+        var mapperInterfaceType = typeof(IMapper<,>);
+
+        foreach (var assembly in assemblies)
+        {
+            var mapperImplementations = assembly.GetTypes()
+                .Where(t => t is { IsClass: true, IsAbstract: false })
+                .SelectMany(t => t.GetInterfaces()
+                    .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == mapperInterfaceType)
+                    .Select(i => new { Interface = i, Implementation = t }));
+
+            foreach (var map in mapperImplementations)
+                services.AddTransient(map.Interface, map.Implementation);
+        }
+
+        services.AddSingleton<IMappingService, MappingService>();
+        return services;
+    }
+}
--- Questionnaire.Application/Registers/Services.cs
+++ Questionnaire.Application/Registers/Services.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Questionnaire.DataAccess.Context;
+using Questionnaire.DataAccess.DomainRepositories;
+using Questionnaire.DataAccess.Models;
+using Questionnaire.DataAccess.UnitOfWork;
+using Questionnaire.SharedKernel.Cqrs;
+using Questionnaire.SharedKernel.Services;
 
 namespace Questionnaire.Application.Registers;
 
@@ -8,8 +15,14 @@ public static class Services
     public static IServiceCollection CollectRepos(this IServiceCollection services)
     {
         services.AddScoped<ApplicationDbContext>();
-        
-        
+        services.AddScoped<IDispatcher, Dispatcher>();
+
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+        services.AddScoped<IJwtService, JwtService>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
         services.BuildServiceProvider();
 
         return services;
